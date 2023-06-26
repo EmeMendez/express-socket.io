8350edbd93ed3770cc2f6e2e517699d498fe231b
emelindamendez <emendez@gmail.com>
2023-06-25 22:48:37 -0400

    Instalación socket.io

---
+++ models/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const config = require('../config/app');
+
+class Server {
+
+    constructor(){
+        this.app = express();
+        this.port = config.PORT;
+        this.server = require('http').createServer(this.app);
+        this.io = require('socket.io')(this.server);
+        this.middlewares();
+        this.routes();
+        this.listen();
+    }
+
+    routes(){
+        // this.app.use('/api/users', require('../routes/users.router'));
+    }
+
+    middlewares(){
+        this.app.use(cors());
+        //Lectura y parseo del body
+        this.app.use(express.json());
+        this.app.use(express.static('public'));
+        // this.app.use('storage', express.static('storage'));
+        // this.app.use(fileUpload({
+        //     useTempFiles : true,
+        //     tempFileDir : '/tmp/',
+        //     createParentPath: true
+        // }));
+    }
+
+    listen(){
+        this.server.listen(this.port, () => {
+            console.log(`Example app listening on port ${this.port}`);
+        })
+    }
+}
+
+module.exports = Server
